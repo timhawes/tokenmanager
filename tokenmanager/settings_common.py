cc3f606074ecfebc3dfcd7ecca2db9d61a401dc3
Tim Hawes <me@timhawes.com>
2025-06-05 11:53:27 +0100

    Add MFA options to allauth dependency

--- tokenmanager/settings_common.py
+++ tokenmanager/settings_common.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.humanize",
     "allauth",
     "allauth.account",
+    "allauth.mfa",
     "allauth.socialaccount",
     "django_bootstrap5",
     "django_extensions",
